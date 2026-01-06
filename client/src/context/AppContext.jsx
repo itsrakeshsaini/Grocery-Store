a3ae6cc016daebe02d31a99130ab4bcd5070b745
Rakesh Saini <itsrakesh.co.in@gmail.com>
2026-01-06 22:32:02 +0530

    Added navigation bar

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,20 +1,22 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
-export const AppProvider = ({children}) => {
-
-    const navigate = userNaviget();
+export const AppContextProvider = ({ children }) => {
+    const navigate = useNavigate();
     const [user, setUser] = useState(null);
     const [isSeller, setIsSeller] = useState(false);
-    const value = {navigate, user, setUser, setIsSeller, isSeller};
+    const [ShowUserLogin, setShowUserLogin] = useState(false);
 
 
-    return <AppContext.Provider value={value}>
-        {children}
-    </AppContext.Provider>
-}
+    const value = { navigate, user, setUser, isSeller, setIsSeller, ShowUserLogin, setShowUserLogin };
 
-export const useAppContext = () => {
-    return useContext(AppContext)
-}
+    return (
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    );
+};
+
+export const useAppContext = () => useContext(AppContext);
